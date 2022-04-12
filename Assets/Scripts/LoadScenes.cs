8f814d2aa26cda89b50b13a9d3d69549b7177aa6
Wojciech Wroński <hierodrimm@gmail.com>
2022-04-12 22:58:09 +0200

    Delete ads

--- Assets/Scripts/LoadScenes.cs
+++ Assets/Scripts/LoadScenes.cs
@@ -1,23 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Advertisements;
 public class LoadScenes : MonoBehaviour
 {
-// #if UNITY_ANDROID
-//     public string gameId = "4146801";
-//     public string surfacingId = "Interstitial_Android";
-// #elif UNITY_IOS
-//     public string gameId = "4146800";
-//     public string surfacingId = "Interstitial_iOS";
-// #endif
     bool testMode = false;
 
-    void Start()
-    {
-        //Advertisement.Initialize(gameId, testMode);
-    }
     public void LoadMenu()
     {
         SceneManager.LoadScene("Menu");
@@ -30,19 +16,6 @@ public class LoadScenes : MonoBehaviour
 
     public void RestartGame()
     {
-        //ShowAd();
         SceneManager.LoadScene("Game");
     }
-
-    // void ShowAd()
-    // {
-    //     if (Random.Range(0,2) == 0 && Advertisement.IsReady(surfacingId))
-    //     {
-    //         Advertisement.Show(surfacingId);
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("Restart ad is not");
-    //     }
-    // }
 }
